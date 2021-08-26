a49a5d251597510c7938663094d1bd1055cd5e2c
Oplish <89449866+Oplish@users.noreply.github.com>
2021-08-27 07:21:39 +0800

    Create Time.py

---
+++ 蟒/Time.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python 
+# -*- coding: utf-8 -*-
+
+import time
+ 
+t = time.localtime(time.time())
+localtime = time.asctime(t)
+str = "北京时间:" + time.asctime(t)
+print("Hello world!");
+print(str);
+c=8;
+input(c);
+int(c);
+c=c+13;
+print(c);
