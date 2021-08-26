bc4ace2fd46a3f32308631cb9269a5d54ed9c902
Oplish <89449866+Oplish@users.noreply.github.com>
2021-08-26 12:06:05 +0800

    Add files via upload

---
+++ C++/Us.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+    cout << "Us" << endl;
+    char a;
+    cin.get(a);
+    cout<<a<<endl;
+    char b;
+    cin>>b;
+    cout<<b;
+} 
