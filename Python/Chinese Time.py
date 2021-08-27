dd743abe42698c5f63874df6d15467288446ed7e
Oplish <89449866+Oplish@users.noreply.github.com>
2021-08-27 12:44:47 +0800

    Create Chinese Time.py

---
+++ Python/Chinese Time.py
@@ -0,0 +1,10 @@
+	#!/usr/bin/python 
+# -*- coding: utf-8 -*-
+
+import time
+ 
+t = time.localtime(time.time())
+localtime = time.asctime(t)
+str = "时间:" + time.asctime(t)
+print("你好世界");
+print(str);
