6b46d2378f4d9762aab79825cc75b90ef12b7e03
Oplish <89449866+Oplish@users.noreply.github.com>
2021-08-29 09:20:24 +0800

    Add files via upload

---
+++ ybt.ssoier.cn:8088/2045.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+int a[30][30]={0};
+int main()
+{
+	int n,d,x,y;
+	cin>>n;
+	memset(a,0,sizeof(a));
+	d=1,x=0,y=n-1;
+	a[x][y]=1; 
+	while(d<n*n)
+	{
+		while(x<n-1&&!a[x+1][y])
+			a[++x][y]=++d;		
+		while(y>0&&!a[x][y-1])
+				a[x][--y]=++d;	
+		while(x>0&&!a[x-1][y])
+			a[--x][y]=++d;
+		while(y<n-1&&!a[x][y+1])
+			a[x][++y]=++d;	
+	}
+	for(int i=0;i<n;i++)
+	{
+		for(int k=0;k<n;k++)
+		{
+			cout<<a[i][k]<<" ";
+		}
+		cout<<endl;
+	}
+	return 0;
+}
+
