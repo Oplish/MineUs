9febdc4e8a97cbe194e104db99f8463399aada56
Oplish <89449866+Oplish@users.noreply.github.com>
2021-08-30 09:24:15 +0800

    Add files via upload

---
+++ C++/Accumulator Carriage.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int Oplish(long long Sort){
+	if(Sort==1){
+		return 1;
+	}
+	if(Sort>=2){
+		return Oplish(Sort-1)+Sort;
+	}
+}
+int main(){
+	long long Fn;
+	cin>>Fn;
+	cout<<Oplish(Fn);
+}
