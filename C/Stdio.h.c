8ebf2ef0feac18505577d216597e1894c8acd109
Oplish <89449866+Oplish@users.noreply.github.com>
2021-09-07 16:03:30 +0800

    Create Stdio.h.c

---
+++ C/Stdio.h.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int n=2;
+    int net=1;
+    int data=1 ;
+    while(printf("C number\n")){
+       scanf("%d",&n);
+       scanf("%d",&net);
+       for(;data<=net;data++){
+          n=n*n+data+n; 
+       }
+       printf("%d\n",n);
+    }
+    return 0;
+}
